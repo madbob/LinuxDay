e674d096169716d687d85ac26c141928fdf3d563
Roberto Guido <bob4job@gmail.com>
2011-09-28 04:08:20 +0200

    Aggiunto script per aggiornare l'account Twitter con le nuove registrazioni sul sito (ancora in formato di test)

---
+++ twitter/publisher.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+wget http://www.linuxday.it/data
+lines=`wc -l data | cut -d' ' -f 1`
+
+tail -n $(($lines - 1)) data > data.tmp
+mv data.tmp data
+
+if [ ! -e olddata ]
+then
+	mv data olddata
+	exit
+fi
+
+while read i
+do
+	i=`echo $i | sed "s/\",\"/#/g"`
+	city=`echo $i | cut -d'#' -f 4`
+
+	if [ `grep "$city" olddata | wc -l` -eq 0 ]
+	then
+		link=`echo $i | cut -d'#' -f 6`
+
+		if [ "$link" != "" ]
+		then
+			link=`python ur1.py $link`
+
+			if [ $? -eq 0 ]
+			then
+				# twidge -c /home/madbob/.twidgerc-linuxday-twitter update "registrato un nuovo LinuxDay a $city: $link"
+				echo "registrato un nuovo LinuxDay a $city: $link"
+			fi
+		fi
+	fi
+done < "data"
+
+mv data olddata
+
